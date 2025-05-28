45b255ebe8143eb09dd0a8d905e906c644f55f95
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-05-28 14:53:13 +0200

    added routes and a json file

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
-import cors from "cors"
-import express from "express"
+import cors from 'cors'
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import fs from 'fs'
+
+const thoughtsData = JSON.parse(fs.readFileSync('./data/thoughts.json', 'utf8'))
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+let messages = thoughtsData
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+// Get API documentation
+app.get('/', (req, res) => {
+  const endpoints = listEndpoints(app)
+  res.send({
+    message: 'Welcome to the Happy Thoughts API',
+    endpoints: endpoints
+  })
 })
+
+// Return all messages
+app.get('/thoughts', (req, res) => {
+  res.json(messages)
+})
+
+// Get messages sorted by hearts
+app.get('/thoughts/trending', (req, res) => {
+  const sortedMessages = [...messages].sort((a, b) => b.hearts - a.hearts)
+  res.json(sortedMessages)
+})
+
+// Get a specific message
+app.get('/thoughts/:id', (req, res) => {
+  const id = req.params.id
+  const message = messages.find((message) => message._id === id)
+  if (message) {
+    res.json(message)
+  } else {
+    res.status(404).send({ error: 'Thought not found' })
+  }
+})
+
+// Create a new message
+app.post('/thoughts', (req, res) => {})
+
+// Increment hearts count
+app.post('/thoughts/:id/like', (req, res) => {})
 
 // Start the server
 app.listen(port, () => {
