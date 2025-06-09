d57bc0a027abd45c92d436c9566c245e4832cca4
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-06-09 21:23:46 +0200

    minor fixes

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import crypto from 'crypto'
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+export default User = mongoose.model('User', userSchema)
